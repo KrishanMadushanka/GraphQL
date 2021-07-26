c6d0397cd8a9c4270d7f596bb9cc2ee4c8b4781d
KrishanMadushanka <wmnkrishanmadushabnka@gmail.com>
2021-07-26 07:53:24 +0530

    fixes

--- app/src/main/java/com/example/graphql/Constants.kt
+++ app/src/main/java/com/example/graphql/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.graphql
 
 object Constants {
-    const val TOKEN = "Bearer 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"
+    const val TOKEN = "Bearer <Your Token>"
     const val QUERY =
         "query{repositoryOwner(login: \"sindresorhus\") { login ... on User { pinnedItems(first: 3) { nodes { ... on Repository { name description stargazerCount primaryLanguage { name } owner { avatarUrl login } } } } topRepositories(first: 10, orderBy: {field: NAME, direction: ASC}) { edges { node { name description stargazerCount primaryLanguage { name } owner { avatarUrl login } } } } email name avatarUrl starredRepositories { totalCount } followers { totalCount } following { totalCount } } }}"
 }
